7d9673311c70eee612bb572c87bb9bbaba204995
JoeBater <36243708+JoeBater@users.noreply.github.com>
2023-12-11 07:24:17 +0000

    initial upload
    an old kaggle competition

---
+++ travelling_santa.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 28 06:13:57 2018
+
+@author: User
+"""
+
+import pandas as pd
+
+route = []
+changes = []
+distances = []
+
+cities = pd.read_csv("cities.csv")
+
+cities.head()
+
